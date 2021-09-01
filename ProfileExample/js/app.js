86d636937658a83de5f60196cba9b10ab46e1aa7
uno1k <dkail@yahoo.ca>
2021-09-01 10:47:29 -0500

    Classwork 09/01/2021
    Calc

--- ProfileExample/js/app.js
+++ ProfileExample/js/app.js
@@ -1,1 +1,6 @@
-
+$('#btntest').click(function() {
+    const decTaxRate = .0925;
+    let decHours = $('#txtHours').val();
+    let decRate = $('#txtPayRate').val();
+    console.log(decHours * decRate);
+});
