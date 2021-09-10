876be92759046d97173b2e2e3221e03a8eb8b60e
uno1k <dkail@yahoo.ca>
2021-09-10 10:09:12 -0500

    school

--- ProfileExample/js/app.js
+++ ProfileExample/js/app.js
@@ -1,6 +1,16 @@
-$('#btntest').click(function() {
+  
+$('#btnTest').click(function() {
     const decTaxRate = .0925;
     let decHours = $('#txtHours').val();
     let decRate = $('#txtPayRate').val();
     console.log(decHours * decRate);
 });
+$('#cboEmployeeType').change(function() {
+    if($('#cboEmployeeType').val() == 'FULL'){
+        //$('#divHours').slideUp();
+        //$('#divHours').css('display','none');
+        $('#divHours').addClass('d-none');
+    }else {
+        $('#divHours').removeClass('d-none').slideDown();
+    }
+})
