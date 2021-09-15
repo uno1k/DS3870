fdc0cd3aae5713e42a42b3683734c6c6f15ba9e4
uno1k <dkail@yahoo.ca>
2021-09-14 23:02:07 -0500

    assignment

--- PigPen/js/app.js
+++ PigPen/js/app.js
@@ -12,7 +12,6 @@ $.getJSON("https://www.swollenhippo.com/getEmployeesByAPIKey.php?APIKey=Mickey20
 })
 function buildEmployeeCard(){
     $.each(arrEmployees,function(i,person){
-        if(person.FirstName != 'John'){
             let strHTML = '<div class="card col-3 offset-5 mt-3">';
             strHTML += '<h3 class="text-center"><a href="mailto:' + person.Email + '">' + person.FirstName + ' ' + person.LastName + '</a></h3>';
             strHTML += '<h4 class="text-center">' + person.Postion +'</h4>';
@@ -21,15 +20,13 @@ function buildEmployeeCard(){
             strHTML += '<p>Hourly Rate: ' + person.HourlyRate + '</p>';
             strHTML += '<div class="form-group">';
             strHTML += '<label class="txtHoursWorked">Hours Worked</label>';
-            strHTML += '<input id="txtHoursWorked">';
+            strHTML += '<input class="txtHoursWorked">';
             strHTML += '</div>';
             strHTML += '<label class="txtTotalPay">Total Pay</label>';
-            strHTML += '<input id="txtTotalPay">';
+            strHTML += '<input class="txtTotalPay">';
             strHTML += '<button id="btnCalculate" type="button" class="btn btn-warning">Calculate Pay</button>'
             strHTML += '</div>';
             $('body').append(strHTML);
-        }
-        
     });
 }
 
