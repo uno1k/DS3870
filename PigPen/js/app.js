a991b92678c1195efc8500e316b0dd342cb66bb8
uno1k <dkail@yahoo.ca>
2021-09-14 22:58:50 -0500

    assignment

--- PigPen/js/app.js
+++ PigPen/js/app.js
@@ -1,24 +1,36 @@
-  
-$('#btnTest').click(function() {
-    const decTaxRate = .0925;
-    let decHours = $('#txtHours').val();
-    let decRate = $('#txtPayRate').val();
-    console.log(decHours * decRate);
-});
-$('#cboEmployeeType').change(function() {
-    if($('#cboEmployeeType').val() == 'FULL'){
-        //$('#divHours').slideUp();
-        //$('#divHours').css('display','none');
-        $('#divHours').addClass('d-none');
-    }else {
-        $('#divHours').removeClass('d-none').slideDown();
-    }
+var arrEmployees;
+$.getJSON("https://www.swollenhippo.com/getEmployeesByAPIKey.php?APIKey=Mickey2021!", function(result){
+    console.log(result);
+    arrEmployees = result;
+    buildEmployeeCard();
+    $.each(result,function(i,person){
+        console.log(person.FirstName);
+        console.log(person.FirstName + ' ' + person.LastName);
+        console.log(person.HireDate);
+        $('#txtEmail').val(person.Email);
+    })
 })
+function buildEmployeeCard(){
+    $.each(arrEmployees,function(i,person){
+        if(person.FirstName != 'John'){
+            let strHTML = '<div class="card col-3 offset-5 mt-3">';
+            strHTML += '<h3 class="text-center"><a href="mailto:' + person.Email + '">' + person.FirstName + ' ' + person.LastName + '</a></h3>';
+            strHTML += '<h4 class="text-center">' + person.Postion +'</h4>';
+            strHTML += '<h4 class="mt-3">Profile Details</h4>';
+            strHTML += '<p>Hire Date: ' + person.HireDate + ' </p>';
+            strHTML += '<p>Hourly Rate: ' + person.HourlyRate + '</p>';
+            strHTML += '<div class="form-group">';
+            strHTML += '<label class="txtHoursWorked">Hours Worked</label>';
+            strHTML += '<input id="txtHoursWorked">';
+            strHTML += '</div>';
+            strHTML += '<label class="txtTotalPay">Total Pay</label>';
+            strHTML += '<input id="txtTotalPay">';
+            strHTML += '<button id="btnCalculate" type="button" class="btn btn-warning">Calculate Pay</button>'
+            strHTML += '</div>';
+            $('body').append(strHTML);
+        }
+        
+    });
+}
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+
